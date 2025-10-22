2bb11441833937151c113f4cdf65c4d599312b51
Alan Anderson <alanandersondev@gmail.com>
2025-10-22 15:37:11 -0300

    Add files via upload

---
+++ src/br/com/desafio/minhasmusicas/principal/Principal.java
@@ -0,0 +1,37 @@
+package br.com.desafio.minhasmusicas.principal;
+
+import br.com.desafio.minhasmusicas.modelos.MinhasPreferidas;
+import br.com.desafio.minhasmusicas.modelos.Musica;
+import br.com.desafio.minhasmusicas.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Imagine Dragon");
+        minhaMusica.setCantor("Warriors");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("Minha Radio");
+        meuPodcast.setApresentador("Leon ");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+    }
+}
